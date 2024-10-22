d5a165718e505c1d4cc739997d974636592078ed
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-10-22 15:24:57 -0300

    Update menu.java
    xd

--- Looping/menu.java
+++ Looping/menu.java
@@ -1,1 +1,125 @@
-d
+package menu;
+
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
+
+public class VentanaPrincipal extends javax.swing.JFrame {
+
+    public VentanaPrincipal() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItemRegistro = new javax.swing.JMenuItem();
+        jMenuItemSaludo = new javax.swing.JMenuItem();
+        jMenuItemSalir = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Ejemplo de Menú");
+
+        jMenu1.setBackground(new java.awt.Color(0, 153, 153));
+        jMenu1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jMenu1.setForeground(new java.awt.Color(204, 255, 204));
+        jMenu1.setText("Archivo");
+        jMenu1.setOpaque(true);
+
+        jMenuItemRegistro.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItemRegistro.setMnemonic(KeyEvent.VK_A);
+        jMenuItemRegistro.setText("Registrarte...");
+        jMenuItemRegistro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemRegistroActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItemRegistro);
+
+        jMenuItemSaludo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItemSaludo.setText("Saludo");
+        jMenuItemSaludo.setEnabled(false);
+        jMenuItemSaludo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemSaludoActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItemSaludo);
+
+        jMenuItemSalir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_X, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItemSalir.setText("Salir");
+        jMenuItemSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemSalirActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItemSalir);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 273, Short.MAX_VALUE)
+        );
+
+        pack();
+    }                     
+
+    private void jMenuItemRegistroActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+       // Método que responde al evento click sobre el JMenuItemRegistro
+        nombre = JOptionPane.showInputDialog(this, "Escribe tu nombre: ");
+        jMenuItemSaludo.setEnabled(true);
+    }                                                 
+
+    private void jMenuItemSaludoActionPerformed(java.awt.event.ActionEvent evt) {                                                
+        // Método que responde al evento click sobre el JMenuItemSaludo
+        JOptionPane.showMessageDialog(this, "Hola " + nombre);
+    }                                               
+
+    private void jMenuItemSalirActionPerformed(java.awt.event.ActionEvent evt) {                                               
+        // Método que responde al evento click sobre el JMenuItemSalir
+        this.dispose();
+    }                                              
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaPrincipal().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItemRegistro;
+    private javax.swing.JMenuItem jMenuItemSalir;
+    private javax.swing.JMenuItem jMenuItemSaludo;
+    public String nombre;
+}
