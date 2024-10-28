81d006b658b3f8f4b6746f6dc8d42d51e74a3211
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-10-28 23:17:10 +0100

    Update Imagen.java
    xd

--- Looping/suicidarme dos/src/banca/Imagen.java
+++ Looping/suicidarme dos/src/banca/Imagen.java
@@ -1,7 +1,7 @@
  //creador de todo
  package banca;
 
-
+//Este archivo sirve para mostrar las 4 imagenes que tenemos
 import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.ImageIcon;
@@ -17,15 +17,14 @@ public class Imagen extends JPanel {
         this.setSize(600, 400);
     }
 
-    // Método para establecer la imagen de fondo
     public void setImagen(String nombreImagen) {
-        // Intentamos cargar la imagen desde el classpath
+        //carga la imagen desde un classpoint
         java.net.URL imageURL = getClass().getResource(nombreImagen);
         if (imageURL != null) {
             imagenFondo = new ImageIcon(imageURL);
         } else {
             System.out.println("Imagen no encontrada: " + nombreImagen);
-            imagenFondo = null; // Para evitar el NullPointerException
+            imagenFondo = null; //por si no se encuentra la imagen
         }
     }
 
@@ -38,52 +37,3 @@ public class Imagen extends JPanel {
         }
     }
 }
-
-
-//Intento medio raro
-/*
-package banca;
-
-import java.awt.Dimension;
-import java.awt.Graphics;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-
-public class Imagen extends JPanel {
-
-    private static final long serialVersionUID = 1L;
-    private ImageIcon imagenFondo;
-    private String nombreImagenActual; // Variable para almacenar el nombre de la imagen actual
-
-    public Imagen(String nombreImagen) {
-        setImagen(nombreImagen);
-        this.setSize(600, 400);
-    }
-
-    // Método para establecer la imagen de fondo
-    public void setImagen(String nombreImagen) {
-        // Intentamos cargar la imagen desde el classpath
-        java.net.URL imageURL = getClass().getResource(nombreImagen);
-        if (imageURL != null) {
-            imagenFondo = new ImageIcon(imageURL);
-            nombreImagenActual = nombreImagen; // Almacena el nombre de la imagen actual
-        } else {
-            System.out.println("Imagen no encontrada: " + nombreImagen);
-            imagenFondo = null; // Para evitar el NullPointerException
-        }
-    }
-
-    // Método para obtener el nombre de la imagen actual
-    public String getImagenNombre() {
-        return nombreImagenActual;
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Dimension tamanio = getSize();
-        if (imagenFondo != null) {
-            g.drawImage(imagenFondo.getImage(), 0, 0, tamanio.width, tamanio.height, this);
-        }
-    }
-}*/
